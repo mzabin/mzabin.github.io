e825b5c877435f3939ce60a1eb0c858ea7a92ff3
mzabin <zabinmastura@gmail.com>
2025-02-13 03:09:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function generateMessage(noCount) {
   const messages = [
     "No",
     "Are you sure?",
-    "Babi pleasee",
+    "Babi pleasee??",
     "Don't do this to me :(",
     "You're breaking my heart",
     "I'm gonna cry...",
